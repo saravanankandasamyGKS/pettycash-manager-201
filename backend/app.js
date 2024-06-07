e821b76d0b34ca622d5271afe16c400e35608a0f
saravanankandasamyGKS <gksaravanan008@gmail.com>
2024-06-07 17:21:07 +0530

    update

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,9 @@ const PORT = process.env.PORT
 
 //middlewares
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    
+}))
 
 //routes
 readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
